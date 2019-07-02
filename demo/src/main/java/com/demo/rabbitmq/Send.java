12bf23f026aed070aa8549a23c348658bc751a9c
mounb <908677314@qq.com>
2019-07-02 14:12:12 +0800

    add rabbitmq

---
+++ demo/src/main/java/com/demo/rabbitmq/Send.java
@@ -0,0 +1,23 @@
+package com.demo.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        //factory.setHost("localhost");
+        factory.setUri("amqp://miner:pecado0930@47.101.183.242:5672");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World from java!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
---
+++ demo/src/test/java/Send2.java
@@ -0,0 +1,21 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send2 {
+
+    private final static String QUEUE_NAME = "queue1";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        //factory.setHost("localhost");
+        factory.setUri("amqp://miner:pecado0930@47.101.183.242:5672");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World from java2!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
